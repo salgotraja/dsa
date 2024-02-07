1fd68cbbbae9e202f4cebf6bf93f5dbfebd30d0b
Jagdish Sagotra <jagdishsal@gmail.com>
2024-02-07 22:56:24 +0530

    code cleanup

--- src/main/java/io/js/arrays/RemoveDuplicate.java
+++ src/main/java/io/js/arrays/RemoveDuplicate.java
@@ -18,10 +18,4 @@ public class RemoveDuplicate {
 
         return index;
     }
-
-    public static void main(String[] args) {
-        int [] nums = {0,0,1,1,1,2,2,3,3,4};
-        System.out.println("\nTotal: " + new RemoveDuplicate().removeDuplicates(nums));
-
-    }
 }
