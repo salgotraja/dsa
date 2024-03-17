6511639131cd32282c306a1ecdbd18b7531c70ed
Jagdish Sagotra <jagdishsal@gmail.com>
2024-03-17 23:32:05 +0530

    cleanup

--- src/main/java/io/js/arrays/HeightChecker.java
+++ src/main/java/io/js/arrays/HeightChecker.java
@@ -37,24 +37,4 @@ public class HeightChecker {
         }
 
     }
-
-    private int[] countingSort(int[] arr) {
-        int max = Arrays.stream(arr).max().getAsInt();
-        int[] count = new int[max + 1];
-
-        for (int num : arr) {
-            count[num]++;
-        }
-
-        for (int i = 1; i < count.length; i++) {
-            count[i] += count[i - 1];
-        }
-
-        int[] output = new int[arr.length];
-        for (int i = arr.length - 1; i >= 0; i--) {
-            output[--count[arr[i]]] = arr[i];
-        }
-
-        return output;
-    }
 }
